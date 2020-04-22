f73c1d286ea9bc56da3defe0679b2e8ecb0c0f5a
RyanAngJY <ryanang.jy@gmail.com>
2020-04-22 16:29:26 +0800

    Minor changes

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
 });
 
 con.connect((err) => {
