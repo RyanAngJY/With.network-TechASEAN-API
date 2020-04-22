968b1d032c5711caf0870a5b95e07d9aa23642a6
RyanAngJY <ryanang.jy@gmail.com>
2020-04-22 19:40:13 +0800

    Only return startups and founders

--- sql/index.js
+++ sql/index.js
@@ -1,10 +1,12 @@
 const sql = {};
 
 sql.query = {
-    // get_courses: 'SELECT * FROM Courses',
-    getEntities: "SELECT * FROM entities LIMIT ?, ?",
+    // Only search entities that are startups or founders
+    getEntities:
+        "SELECT * FROM entity_roles INNER JOIN entities ON entity_id = entities.id WHERE role_id = 59 OR role_id = 72 LIMIT ?, ?",
     getEntity: "SELECT * FROM entities WHERE id = ?",
-    searchEntities: "SELECT * FROM entities WHERE name LIKE ? LIMIT ?, ?",
+    searchEntities:
+        "SELECT * FROM entity_roles INNER JOIN entities ON entity_id = entities.id WHERE (role_id = 59 OR role_id = 72) AND name LIKE ? LIMIT ?, ?",
 };
 
 module.exports = sql;
